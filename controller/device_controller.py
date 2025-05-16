6768eea15ab32c3c1245ceb33d1947c614a97361
rasptai <rasptai@gmail.com>
2025-05-16 12:40:51 +0900

    4

--- controller/device_controller.py
+++ controller/device_controller.py
@@ -1,6 +1,10 @@
-from device import Device, HardID
+from enum import IntEnum
+from monitor import Monitor
 from signature import hardware_lib
 
+class HardID(IntEnum):
+    SLX = 90010
+    IMG = 90050
 class DeviceController:
     _instance = None
     _initialized = False
@@ -14,15 +18,16 @@ class DeviceController:
     def __init__(self):
         if self._initialized:
             return
-        self.slx = Device(HardID.SLX)
-        self.img = Device(HardID.IMG)
+        self.monitors = {}
+        for hard in {HardID.SLX, HardID.IMG}:
+            self.monitors[hard] = Monitor(hard, self._on_state)
         self._initialized = True
 
     def __enter__(self):
         if not self._context_count:
-            self.FuncDeviceOpen()
-            self.slx.start_monitor()
-            self.img.start_monitor()
+            self.device_open()
+            for monitor in self.monitors.values():
+                monitor.start()
             print("Devices are opened and started.")
 
         self._context_count += 1
@@ -31,17 +36,31 @@ class DeviceController:
     def __exit__(self, exc_type, exc_value, traceback):
         self._context_count -= 1
         if not self._context_count:
-            self.slx.stop_monitor()
-            self.img.stop_monitor()
-            self.FuncDeviceClose()
+            for monitor in self.monitors.values():
+                monitor.stop()
+            self.device_close()
             print("Devices are stopped and closed.")
-
         return False
 
-    def FuncDeviceOpen(self):
+    def _on_state(self, state):
+        self.state = state
+        if state == 0:
+            print(f"{self.hard} is Ready.")
+        elif state == 1:
+            print(f"{self.hard} is System Busy.")
+        elif state == 2:
+            print(f"{self.hard} is Busy.")
+        elif state == 3:
+            print(f"{self.hard} is Stop.")
+        elif state == 4:
+            print(f"{self.hard} is Collision Detected.")
+        elif state == 5:
+            print(f"{self.hard} is Collision Stop.")
+
+    def device_open(self):
         result = hardware_lib.FuncDeviceOpen()
         if result != 0:
             raise Exception(f"Failed to open device. Error code: {result}")
 
-    def FuncDeviceClose(self):
+    def device_close(self):
         hardware_lib.FuncDeviceClose()
